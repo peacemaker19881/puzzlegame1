f036ab0e9e764413114366eeb6fa88764c6338b6
Peacemaker1988 <jeandelapaix2013@gmail.com>
2023-02-22 00:59:12 -0800

    LASTCOMMIT

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/BUMBA LOGO.png">
+  <HelloWorld msg="Murakaza neza muri game development"/>
 </template>
 
 <script>
